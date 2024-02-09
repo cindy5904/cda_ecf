1a0c8f6c553ff3562d0f2e5ff8947256cbb67516
Cindy Telmon <telcindy59@gmail.com>
2024-02-09 12:08:14 +0100

    fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,10 +5,10 @@ function isAuthenticated(req, res, next) {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
     console.log(decodedToken)
-    const userId = decodedToken.id_user;
+    const userId = decodedToken.user_id;
     console.log("ID de l'utilisateur extrait:", userId);
     req.auth = {
-      id_user: userId,
+      user_id: userId,
     };
     return next();
   } catch (error) {
